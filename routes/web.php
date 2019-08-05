588af6b90b1cfd796b1a466991b8b3203b33e01a
Misxhka <misxhka@gmail.com>
2019-08-05 21:04:36 +0800

    w.i.p

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/email', function () {
 Route::post('follow/{user}', 'FollowsController@store');
 
 Route::get('/', 'PostsController@index');
-Route::get('/p/create', 'PostsController@create')->name('p.create');
+Route::get('/p/create', 'PostsController@create');
 Route::post('/p', 'PostsController@store');
 Route::get('/p/{post}', 'PostsController@show');
 
